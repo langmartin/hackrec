54c18d454f710022285d6411aee1452f20c56515
Lang Martin <lang.martin@gmail.com>
2011-12-12 20:32:37 -0500

    new 03 CS61A Lectures 1-3.scm

---
+++ 03 CS61A Lectures 1-3.scm
@@ -0,0 +1,29 @@
+(load "/Users/lang/code/hackrec/sentences.scm")
+
+(define (reverse word)
+  (if (empty? word)
+      '()
+      (sentence (last word)
+                (reverse (butlast word)))))
+
+(sentence 'o
+          (sentence 'o
+                    (sentence 'f
+                              '())))
+
+(define (%rev word answer)
+  (if (empty? word)
+      answer
+      (%rev (butlast word)
+            (sentence answer
+                      (last word)))))
+
+(define (reverse word)
+  (%rev word '()))
+
+(reverse 'foo)
+
+(%rev 'foo '())
+(%rev 'fo  '(() o))
+(%rev 'f   '(() o o))
+(%rev '()  '(() o o f))
