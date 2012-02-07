5edf96326c191ffd7e39b55671f8ac6f13f4ab72
Lang Martin <lang.martin@gmail.com>
2012-02-07 11:09:24 -0500

    github hides todo keywords, so Exercise as a keyword is a bad idea.

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((org-mode
+  (org-todo-keywords quote
+                     ((type "Exercise" "Optional")))))
